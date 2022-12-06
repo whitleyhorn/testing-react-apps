d1765b8b505fc804b268bdf7b66f272315192270
Whitley Horn <whitleyblaine@protonmail.com>
2022-12-06 13:04:04 -0500

    Add little comment

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -12,8 +12,10 @@ import Counter from '../../components/counter'
 global.IS_REACT_ACT_ENVIRONMENT = true
 
 test('counter increments and decrements when the buttons are clicked', () => {
+  // Emulating the developer user experience
   const div = document.createElement('div')
   document.body.append(div)
+
   const root = createRoot(div)
   act(() => root.render(<Counter />))
   const [decrement, increment] = div.querySelectorAll('button')
