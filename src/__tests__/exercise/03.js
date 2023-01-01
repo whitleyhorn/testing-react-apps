cb383af76e6558089b319c6c870a64d6a1d26177
Whitley Horn <whitleyblaine@protonmail.com>
2023-01-01 01:54:31 -0500

    Finish ex 03 and extra credit

--- src/__tests__/exercise/03.js
+++ src/__tests__/exercise/03.js
@@ -6,14 +6,16 @@ import {screen, render} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Counter from '../../components/counter'
 
-test('counter increments and decrements when the buttons are clicked', () => {
+test('counter increments and decrements when the buttons are clicked', async () => {
   render(<Counter />)
-  const [decrement, increment] = screen.getAllByRole('button')
-  const message = screen.getByText('Current count:', {exact: false})
+
+  const decrement = screen.getByRole('button', {name: /decrement/i})
+  const increment = screen.getByRole('button', {name: /increment/i})
+  const message = screen.getByText(/current count/i)
 
   expect(message).toHaveTextContent('Current count: 0')
-  userEvent.click(increment)
+  await userEvent.click(increment)
   expect(message).toHaveTextContent('Current count: 1')
-  userEvent.click(decrement)
+  await userEvent.click(decrement)
   expect(message).toHaveTextContent('Current count: 0')
 })
