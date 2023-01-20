ce7b1d37a712339c0f4289c9dbe6d717719ac1d3
Whitley Horn <whitleyblaine@protonmail.com>
2023-01-19 19:36:13 -0500

    Finish ex 05 extra credit 03 + 04

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -4,6 +4,7 @@
 import * as React from 'react'
 import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import {rest} from 'msw'
 import {build, fake} from '@jackfranklin/test-data-bot'
 import {setupServer} from 'msw/node'
 import {handlers} from '../../test/server-handlers.js'
@@ -20,6 +21,8 @@ const server = setupServer(...handlers)
 
 beforeAll(() => server.listen())
 afterAll(() => server.close())
+// remove added handlers between tests to preserve test isolation and restore the original handlers
+afterEach(() => server.resetHandlers())
 
 test(`logging in displays the user's username`, async () => {
   render(<Login />)
@@ -43,7 +46,29 @@ test('omitting a password results in an error', async () => {
 
   await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
 
-  expect(
-    screen.getByRole('alert', {text: /password required/i}),
-  ).toBeInTheDocument()
+  expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
+    `"password required"`,
+  )
+})
+
+test('unknown server error displays the error message', async () => {
+  const testErrorMessage = 'something is wrong'
+  // one-off server failure
+  server.use(
+    rest.post(
+      // note that it's the same URL as our app-wide handler
+      // so this will override the other.
+      'https://auth-provider.example.com/api/login',
+      async (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json({message: testErrorMessage}))
+      },
+    ),
+  )
+
+  render(<Login />)
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert')).toHaveTextContent(testErrorMessage)
 })
