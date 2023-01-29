f72aa9988930808f8ee0054b92c900958089a3a9
Whitley Horn <whitleyblaine@protonmail.com>
2023-01-29 02:16:18 -0500

    Use test-utils which is set up the way KCD recommends using react testing library

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -2,19 +2,11 @@
 // http://localhost:3000/easy-button
 
 import * as React from 'react'
-import {render as rtlRender, screen} from '@testing-library/react'
-import {ThemeProvider} from '../../components/theme'
+import {render, screen} from '../../test/test-utils'
 import EasyButton from '../../components/easy-button'
 
-function render(ui, initialTheme = 'light', options = {}) {
-  const Wrapper = ({children}) => (
-    <ThemeProvider initialTheme={initialTheme}>{children}</ThemeProvider>
-  )
-  return rtlRender(ui, {wrapper: Wrapper, ...options})
-}
-
 test('renders with the light styles for the light theme', () => {
-  render(<EasyButton>Easy</EasyButton>, 'light')
+  render(<EasyButton>Easy</EasyButton>, {theme: 'light'})
   const easyBtn = screen.getByRole('button', {name: /easy/i})
   expect(easyBtn).toHaveStyle(`
     background-color: white;
@@ -23,7 +15,7 @@ test('renders with the light styles for the light theme', () => {
 })
 
 test('renders with the dark styles for the dark theme', async () => {
-  render(<EasyButton>Easy</EasyButton>, 'dark')
+  render(<EasyButton>Easy</EasyButton>, {theme: 'dark'})
   const easyBtn = screen.getByRole('button', {name: /easy/i})
   expect(easyBtn).toHaveStyle(`
     background-color: black;
