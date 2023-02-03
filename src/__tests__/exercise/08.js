61eeabc0281b4d1998105db524596f742d39b79c
Whitley Horn <whitleyblaine@protonmail.com>
2023-02-03 18:25:08 -0500

    Finish ex 08 extra credit 02

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -2,28 +2,43 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {render, screen, act} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import {render, act} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
-test('exposes the count and increment/decrement functions', async () => {
-  let counter
-  function TestComponent() {
-    counter = useCounter()
+function setupCounter({initialProps} = {}) {
+  const counter = {}
+  function Counter() {
+    counter.current = useCounter(initialProps)
     return null
   }
+  render(<Counter />)
+  return counter
+}
 
-  render(<TestComponent />)
-  let {increment, decrement} = counter
-  expect(counter.count).toBe(0)
-  act(() => increment())
-  expect(counter.count).toBe(1)
-  act(() => decrement())
-  expect(counter.count).toBe(0)
+test('exposes the count and increment/decrement functions', async () => {
+  const counter = setupCounter()
+
+  expect(counter.current.count).toBe(0)
+  act(() => counter.current.increment())
+  expect(counter.current.count).toBe(1)
+  act(() => counter.current.decrement())
+  expect(counter.current.count).toBe(0)
 })
 
-test('allows customization of the initial count', async () => {})
+test('allows customization of the initial count', async () => {
+  const initialCount = 4
+  const counter = setupCounter({initialProps: {initialCount}})
+  expect(counter.current.count).toBe(initialCount)
+})
 
-test('allows customization of the step', async () => {})
+test('allows customization of the step', async () => {
+  const step = 2
+  const counter = setupCounter({initialProps: {step}})
+  expect(counter.current.count).toBe(0)
+  act(() => counter.current.increment())
+  expect(counter.current.count).toBe(2)
+  act(() => counter.current.decrement())
+  expect(counter.current.count).toBe(0)
+})
 
 /* eslint no-unused-vars:0 */
